3c9294b14e0cc3eceb4bddef92f7c46da6687f45
Andre Yusuki <andre.yusuki@bsgi.org.br>
2012-05-14 16:22:33 -0300

    arquivos do bootstrap completo

--- circulante/catalogo/urls.py
+++ circulante/catalogo/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^pubs/$', 'catalogo.views.listar_publicacoes'),
     url(r'^pub/(\d+)$', 'catalogo.views.ficha_publicacao'),
-    url(r'^hora/$', 'catalogo.view.hora_atual', name = 'hora'),
+    url(r'^hora/$', 'catalogo.views.hora_atual', name = 'hora'),
     url(r'^hora/(-?\d\d?)$', 'catalogo.views.hora_atual_delta', name='hora-caminho'),
 )
